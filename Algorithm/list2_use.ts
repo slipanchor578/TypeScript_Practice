5fad9316f3d3f10a9add36e925a70256ecadadb1
slipanchor578 <slipanchor578@gmail.com>
2022-07-08 17:01:30 +0900

    ジェネリックな自作単方向リストの使用

--- Algorithm/list2_use.ts
+++ Algorithm/list2_use.ts
@@ -1,11 +1,18 @@
 import {LinkedList2} from "./list2";
 
+// stringを値に持つ単方向リストを作成
 const list2 = new LinkedList2<string>();
 
-list2.add("foo");
+for(let i = 0; i < 10; ++i)
+{
+    list2.add(i.toString());
+}
 
-list2.add("bar");
+console.log(list2.toString()); // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 
-console.log(list2.getFirstNode());
+// index = 5 の位置の子ノードの後ろに「Hello」を値に持つ子ノードを挿入する
+list2.insertAfter(5, "Hello");
 
-console.log(list2.toString()); // foo, bar
+console.log(list2.toString()); // 0, 1, 2, 3, 4, 5, hello, 6, 7, 8, 9
+
+console.log(list2.getLength()); // 11
