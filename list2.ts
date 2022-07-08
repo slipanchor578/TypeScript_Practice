472d087dd512c14b9e598d4fa6de7677b782509a
slipanchor578 <slipanchor578@gmail.com>
2022-07-08 10:53:24 +0900

    ジェネリクスな単方向リストの構造

---
+++ list2.ts
@@ -0,0 +1,87 @@
+// ジェネリクス版
+// Object.prototype.toStringを使いたいので「T型はObjectを継承している」事を示している
+class ChildNode2<T extends Object>
+{
+    value: T;
+
+    nextNode: ChildNode2<T> | null = null;
+
+    constructor(value: T)
+    {
+        this.value = value;
+    }
+}
+
+class LinkedList2<T extends Object>
+{
+    #firstNode: ChildNode2<T> | null;
+
+    constructor(){this.#firstNode = null;}
+
+    getFirstNode(): ChildNode2<T> | null
+    {
+        if(this.#firstNode === null)
+        {
+            return null;
+        }
+
+        return this.#firstNode;
+    }
+
+    getLastNode(): ChildNode2<T> | null
+    {
+        if(this.#firstNode === null)
+        {
+            return null;
+        }
+
+        let node = this.#firstNode;
+
+        while(node.nextNode !== null)
+        {
+            node = node.nextNode;
+        }
+
+        return node;
+    }
+
+    add(value: T): void
+    {
+        let node = new ChildNode2(value);
+
+        if(this.#firstNode === null)
+        {
+            this.#firstNode = node;
+
+            return;
+        }
+
+        let lastNode = this.getLastNode();
+
+        lastNode!.nextNode = node;
+    }
+
+    toString(): string
+    {
+        if(this.#firstNode === null)
+        {
+            return "";
+        }
+
+        let node = this.#firstNode;
+
+        // [T extends Object]をしないと、「T型はtoStringメソッドを持っていない」とかエラーが出る
+        let str: string = node.value.toString();
+
+        while(node.nextNode !== null)
+        {
+            node = node.nextNode;
+
+            str = str + ", " + node.value.toString();
+        }
+
+        return str;
+    }
+}
+
+export {LinkedList2};
